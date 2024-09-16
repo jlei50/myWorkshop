73977957d94a55e31ed6db174ca86de037e41dc7
Jady Lei <jlei50@stuy.edu>
2024-09-16 10:49:04 -0400

    added krewes

---
+++ 04_choose/krewes.py
@@ -0,0 +1,29 @@
+# Jady Lei
+# 45
+# SoftDev
+# K04 -- Python Dictionaries
+# 2024-09-16
+# time spent: 
+
+import random
+
+krewes = {
+           4: [ 
+		'DUA','TAWAB','EVA','JACK','VICTOR','EVAN','JASON','COLYI','IVAN','TANZEEM',
+		'TAHMIM','STANLEY','LEON','NAOMI','NIA','ANASTASIA','JADY','BRIAN','JACOB',
+		'ALEX','CHONGTIAN','DANNY','MARCO','ABIDUR','ANKITA','ANDY','ETHAN','AMANDA',
+		'AIDAN','LINDA','QIANJUN','JIAYING','KISHI'
+		],
+           5: [ 
+                'ADITYA','MARGIE','RACHEL','ALEXANDER','ZIYAD','DANNY','ENDRIT','CADEN',
+                'VEDANT','SUHANA','KYLE','KEVIN','RAYMOND','CHRISTOPHER','JONATHAN','SASHA',
+                'NAFIYU','TIM','WILL','DANIEL','BENJAMIN','CLAIRE','CHLOE','STELLA','TRACY',
+                'JESSICA','JACKIE','WEN YUAN','YINWEI','TIFFANY','JAYDEN DANIEL','PRINCEDEN' 
+              ]
+         }
+
+def randomStudent():
+    key = random.randint(4,5)
+    return krewes[key][random.randint(0, len(krewes[key]))]
+
+print(randomStudent())
