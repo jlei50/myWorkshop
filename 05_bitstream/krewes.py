2478d19546a44662d65fbfc13fe06473dd7b8a60
Jady Lei <jlei50@stuy.edu>
2024-09-17 11:01:39 -0400

    almost done

--- 05_bitstream/krewes.py
+++ 05_bitstream/krewes.py
@@ -14,14 +14,15 @@ f = open("krewes.txt", "r")
 raw = f.read()
 students = raw.split("@@@")
 
-studentDict = {4: list(), 5: list()}
+studentDict = {4: [], 5: []}
 
 for student in students:
     student = student.split("$$$")
-    student[0] = int(student[0])
-    studentDict[student[0]].append(list(student[1], student[2]))
+    if len(student) > 1:
+        studentDict[int(student[0])].append([student[1], student[2]])
 
 
-randnum = randrange(studentDict[4])
+randnum = random.randint(0, len(studentDict[4]))
 student = studentDict[4][randnum]
-print("period: " + student[0] + " name: " + student[1] + " ducky name: " + student[2])
+print(student)
+# print("period: " + student[0] + " name: " + student[1] + " ducky name: " + student[2])
