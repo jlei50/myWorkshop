014f08f34b69f9d0783deb47b1c4757e6f06109f
Jady Lei <jlei50@stuy.edu>
2024-09-19 10:54:12 -0400

    finished 06

---
+++ 06_py-csv/numbercruncher.py
@@ -0,0 +1,23 @@
+# Jady Lei
+# 31
+# SoftDev
+# K06 -- python csv files
+# 2024-09-16
+# time spent:
+# DISCO: 
+# QCC: 
+# OPS SUMMARY:
+
+import csv
+import random
+
+with open("occupations.csv", newline="") as csvfile:
+    #creates a dictionary for every row that can be parsed through
+    reader = csv.DictReader(csvfile)
+    jobs = []
+    percents = []
+    for row in reader:
+        jobs.append(row['Job Class']), percents.append(float(row['Percentage']))
+
+# random.choices returns a list, [:-1] to ignore last row, k is returned list size
+print(random.choices(jobs[:-1], weights=percents[:-1], k=1)[0])
