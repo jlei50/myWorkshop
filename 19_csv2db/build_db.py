cfaec7ea191f9581e968d751b8d39a23ba49658d
Jady Lei <jlei50@stuy.edu>
2024-10-18 12:16:30 -0400

    check if tables exist already

--- 19_csv2db/build_db.py
+++ 19_csv2db/build_db.py
@@ -1,6 +1,5 @@
 # Jady Lei
 # 45
-# SoftDev
 # K19 SQL
 # 2024-10-18
 
@@ -12,35 +11,35 @@ DB_FILE="discobandit.db"
 db = sqlite3.connect(DB_FILE) 
 c = db.cursor() #facilitate db ops
 
-c.execute("DROP TABLE courses")
-c.execute("DROP TABLE students")
+coursesExist = (c.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='courses'").fetchall() == [])
+if coursesExist:
+        command = "CREATE TABLE courses(code, mark, id)"
+        c.execute(command)
+        with open("courses.csv", newline="") as csvfile:
+                #creates a dictionary for every row that can be parsed through
+                coursesRaw = csv.DictReader(csvfile)
+                for course in coursesRaw:
+                        command = f"INSERT INTO courses VALUES (?,?,?)"
+                        # {course['code']}, {course['mark']}, {course['id']}
+                        val = (course['code'], course['mark'], course['id'])
+                        c.execute(command, val)
 
-command = "CREATE TABLE courses(code, mark, id)"
-c.execute(command)
-with open("courses.csv", newline="") as csvfile:
-    #creates a dictionary for every row that can be parsed through
-    coursesRaw = csv.DictReader(csvfile)
-    for course in coursesRaw:
-        command = f"INSERT INTO courses VALUES (?,?,?)"
-        # {course['code']}, {course['mark']}, {course['id']}
-        val = (course['code'], course['mark'], course['id'])
-        c.execute(command, val)
-    
-command = "CREATE TABLE students (name, age, id)"
-c.execute(command)
-with open("students.csv", newline="") as csvfile:
-    #creates a dictionary for every row that can be parsed through
-    studentsRaw = csv.DictReader(csvfile)
-    for student in studentsRaw:
-        command = f"INSERT INTO students VALUES (?,?,?)"
-        val = (student['name'], student['age'], student['id'])
-        c.execute(command, val)
+studentsExist = (c.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='students'").fetchall() == [])
+if studentsExist:    
+        command = "CREATE TABLE students (name, age, id)"
+        c.execute(command)
+        with open("students.csv", newline="") as csvfile:
+        #creates a dictionary for every row that can be parsed through
+                studentsRaw = csv.DictReader(csvfile)
+                for student in studentsRaw:
+                        command = f"INSERT INTO students VALUES (?,?,?)"
+                        val = (student['name'], student['age'], student['id'])
+                        c.execute(command, val)
 
 
 db.commit() #save changes
 for row in c.execute("SELECT id, code FROM courses"):
         print(row)
-print()
 print()
 print()
 for row in c.execute("SELECT id, name FROM students"):
